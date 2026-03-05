037c41212e3c8838df5654084024c2e482ae07ae
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2026-03-05 07:41:23 +0100

    TestBases

--- Portamical.xUnit/TestBases/TestBase.cs
+++ Portamical.xUnit/TestBases/TestBase.cs
@@ -2,13 +2,20 @@
 // Copyright (c) 2026. Csaba Dudas (CsabaDu)
 
 using Portamical.xUnit.Converters;
+using Portamical.xUnit.DataProviders;
 
 namespace Portamical.xUnit.TestBases;
 
 public abstract class TestBase : Portamical.TestBases.TestBase
 {
-    protected static TheoryData<TTestData> Convert<TTestData>(
+    protected static TheoryTestData<TTestData> Convert<TTestData>(
+        IEnumerable<TTestData> testDataCollection,
+        ArgsCode argsCode)
+    where TTestData : notnull, ITestData
+    => testDataCollection.ToTheoryTestData(argsCode);
+
+    protected static TheoryTestData<TTestData> Convert<TTestData>(
         IEnumerable<TTestData> testDataCollection)
     where TTestData : notnull, ITestData
-    => testDataCollection.ToTheoryData();
+    => Convert(testDataCollection, ArgsCode);
 }
---
+++ Portamical.xUnit/TestBases/TheoryData/TestBase.cs
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright (c) 2026. Csaba Dudas (CsabaDu)
+
+using Portamical.xUnit.Converters;
+
+namespace Portamical.xUnit.TestBases.TheoryData;
+
+public abstract class TestBase : Portamical.TestBases.TestBase
+{
+    protected static TheoryData<TTestData> Convert<TTestData>(
+        IEnumerable<TTestData> testDataCollection)
+    where TTestData : notnull, ITestData
+    => testDataCollection.ToTheoryData();
+}
