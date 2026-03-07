7daec9d37ebd46c41d846759d1d4c77cfeb5676f
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2026-03-07 20:45:59 +0100

    #pragma warning disable IDE0060

--- Portamical.xUnit/Converters/TestDataConverter.cs
+++ Portamical.xUnit/Converters/TestDataConverter.cs
@@ -10,7 +10,11 @@ public static class TestDataConverter
     internal static TestDataProvider<TTestData> InitTestDataProvider<TTestData>(
         TTestData testData,
         ArgsCode argsCode,
+#pragma warning disable IDE0060
+// testMethodName is used here only for having the same signature
+// as the one in 'TestDataProviderFactory'.
         string? testMethodName = null)
+#pragma warning restore IDE0060
     where TTestData : notnull, ITestData
     => new(testData, argsCode);
 }
