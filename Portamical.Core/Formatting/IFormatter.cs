1b38b0d9753c679c2860df5ef443d623fe7cb19f
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2026-06-10 08:27:03 +0200

    Add IFormatter interfaces and base Formatter<T> class
    Introduced IFormatter and IFormatter<T> for object formatting contracts. Added abstract Formatter<T> base class with helper methods for type-specific formatters. Updated project file to include TypeFormatters folder.

---
+++ Portamical.Core/Formatting/IFormatter.cs
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright (c) 2026. Csaba Dudas (CsabaDu)
+
+namespace Portamical.Core.Formatting;
+
+public interface IFormatter
+{
+    string? Format(object value);
+}
+
+public interface IFormatter<in T> : IFormatter
+{
+    string? Format(T value);
+}
---
+++ Portamical.Core/Formatting/Model/Formatter.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright (c) 2026. Csaba Dudas (CsabaDu)
+
+namespace Portamical.Core.Formatting.Model;
+
+public abstract class Formatter<T> : IFormatter<T>
+{
+    public abstract string? Format(T value);
+
+    string? IFormatter.Format(object value)
+    => Format((T)value);
+
+    protected static string? Format<TContext>(
+        Func<TContext, string?> toString,
+        TContext context)
+    => toString(context);
+}
